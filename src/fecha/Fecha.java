f3b672a2d613bbb6ab25c634417ff291d9b26d5d
203p21 <203p21@203p17-PC>
2017-02-10 09:45:20 +0100

    segundo punto

--- src/fecha/Fecha.java
+++ src/fecha/Fecha.java
@@ -14,6 +14,12 @@ public class Fecha {
         int dia;
         int mes;
         int anio;
+
+    public Fecha(int dia, int mes, int anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
     
     
 }
